c105e591cbd296263272e647abe551da8b546d55
bgq527@mocs.utc.edu <bgq527@mocs.utc.edu>
2020-12-10 15:20:08 +0000

    Updated restarter to account for empty values

--- restart_server.py
+++ restart_server.py
@@ -4,7 +4,12 @@ s = subprocess.check_output("ps ax|grep gunicorn", shell=True)
 s = s.decode("utf-8").split("\n")
 
 for nl in s:
-	pid = nl.split(" ")[0]
+	nl = nl.split(' ')
+	nl = [v for v in nl if v != '']
+	try:
+		pid = nl[0]
+	except IndexError:
+		continue
 	subprocess.run(f"kill -9 {pid}", shell=True)
 
 subprocess.run("authbind gunicorn -w 3 --threads 2 -k gevent -b 0.0.0.0:80 server:app", shell=True)
