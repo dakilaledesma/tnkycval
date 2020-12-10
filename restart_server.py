a170a7a4b96ad61590db9bdc2f5eb13df995f48e
bgq527@mocs.utc.edu <bgq527@mocs.utc.edu>
2020-12-10 01:30:40 +0000

    Scheduled cron restarting

---
+++ restart_server.py
@@ -0,0 +1,10 @@
+import subprocess
+
+s = subprocess.check_output("ps ax|grep gunicorn", shell=True)
+s = s.decode("utf-8").split("\n")
+
+for nl in s:
+	pid = nl.split(" ")[0]
+	subprocess.run(f"kill -9 {pid}", shell=True)
+
+subprocess.run("authbind gunicorn -w 3 --threads 2 -k gevent -b 0.0.0.0:80 server:app", shell=True)
---
+++ restart_server.sh
@@ -0,0 +1,4 @@
+source ~/miniconda3/bin/activate cval && \
+cd ~/shaw-cval-form-flask && \
+python -u /home/dakilaledesma/shaw-cval-form-flask/restart_server.py &
+
